8ce2908c62ba2ca1092ef408cc2f72b58361c47a
NahyeonKim <parakise13@gmail.com>
2022-06-24 23:03:07 +0900

    setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,33 @@
-console.log('Client side javascript file is loaded!')
+console.log("Client side javascript file is loaded!");
 
 // fetch('http://puzzle.mead.io/puzzle').then((res) => {
 // 	res.json().then((data => {
 // 		console.log(data)
 // 	}))
 // })
-const weatherForm = document.querySelector('form')
-const search = document.querySelector('input')
-const messageOne = document.querySelector('#message-1')
-const messageTwo = document.querySelector('#message-2')
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
 
-weatherForm.addEventListener('submit', (e) => {
-    e.preventDefault()
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    const location = search.value
+  const location = search.value;
 
-    messageOne.textContent = 'Loading...'
-    messageTwo.textContent = ''
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
-        response.json().then((data) => {
-            if (data.error) {
-                messageOne.textContent = data.error
-            } else {
-                messageOne.textContent = data.location
-                messageTwo.textContent = data.forecast
-            }
-        })
-    })
-})
+  // localhost가 아닌 heroku url을 이용하기 위해서는 앞의 url을 빼고 /만 있어됨
+  // fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
+});
